023feef24026844dfb576afd613b765ea8a5af6c
tharindunadeeshan <57563360+tharindunadeeshan@users.noreply.github.com>
2022-08-30 21:31:40 +0530

    made some changes

---
+++ src/components/BlogPage/BlogPageCart.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import './BlogPageCart.css'
+
+const BlogPageCart = () => {
+  return (
+    <div className="Blog-Container">
+        <div className="BlogCart">
+            <p className='welcome'>Welcome to Our </p>
+            <p className='Blogss'>Blogs</p>
+            
+        </div>
+        <div className="BlogCart">
+          
+            
+        </div>
+        <div className="BlogCart">
+            <p className='tag1'>Technology | Trends</p>
+            <h1 className='tag2'>Big Data In future</h1>
+            <p className='tag3'>Jane Smith</p>
+            <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br/>
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            
+        </div>
+        <div className="BlogCart">
+        <p className='tag1'>Technology | Trends</p>
+            <h1 className='tag2'>Big Data In future</h1>
+            <p className='tag3'>Jane Smith</p>
+            <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br/>
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+
+        </div>
+        <div className="BlogCart">
+        <p className='tag1'>Technology | Trends</p>
+            <h1 className='tag2'>Big Data In future</h1>
+            <p className='tag3'>Jane Smith</p>
+            <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br/>
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua.. </p>
+        </div>
+        <div className="BlogCart">
+        <p className='tag1'>Technology | Trends</p>
+            <h1 className='tag2'>Big Data In future</h1>
+            <p className='tag3'>Jane Smith</p>
+            <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br/>
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+
+        </div>
+        <div className="BlogCart">
+        <p className='tag1'>Technology | Trends</p>
+            <h1 className='tag2'>Big Data In future</h1>
+            <p className='tag3'>Jane Smith</p>
+            <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br/>
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+
+        </div>
+        <div className="BlogCart">
+        <p className='tag1'>Technology | Trends</p>
+            <h1 className='tag2'>Big Data In future</h1>
+            <p className='tag3'>Jane Smith</p>
+            <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br/>
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+
+        </div>
+    </div>
+  )
+}
+
+export default BlogPageCart
