daa90116e89da9c70d1c84cb8251e62fd0b6ce29
tharindunadeeshan <57563360+tharindunadeeshan@users.noreply.github.com>
2022-08-31 15:46:31 +0530

    text write

--- src/components/BlogPage/BlogPageCart.jsx
+++ src/components/BlogPage/BlogPageCart.jsx
@@ -8,6 +8,7 @@ const BlogPageCart = () => {
         <div className="BlogCart">
             <p className='welcome'>Welcome to Our </p>
             <p className='Blogss'>Blogs</p>
+            <p className='blogtag6'>07 JUL 2022</p>
           
             
         </div>
@@ -16,53 +17,76 @@ const BlogPageCart = () => {
             
         </div>
         <div className="BlogCart">
+            <div className="details">
             <p className='tag1'>Technology | Trends</p>
             <h1 className='tag2'>Big Data In future</h1>
             <p className='tag3'>Jane Smith</p>
             <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br/>
             do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-           <p className='tag4'><FaRegHeart/> <span></span> 600 <span><FaRegCommentDots/><span></span> 50</span></p> 
-            <p className='tag4'>06 JUL 22</p>
-            
+           <p className='tag5'><FaRegHeart/> <span></span> 600 <span><FaRegCommentDots/><span></span> 50</span></p> 
+           <div className='vl'></div>
+            <p className='tag6'>06 JUL 22</p>
+            </div>
         </div>
         <div className="BlogCart">
-        <p className='tag1'>Technology | Trends</p>
+        <div className="details">
+            <p className='tag1'>Technology | Trends</p>
             <h1 className='tag2'>Big Data In future</h1>
             <p className='tag3'>Jane Smith</p>
             <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br/>
             do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-
+           <p className='tag5'><FaRegHeart/> <span></span> 600 <span><FaRegCommentDots/><span></span> 50</span></p> 
+           <div className='vl'></div>
+            <p className='tag6'>06 JUL 22</p>
+            </div>
         </div>
         <div className="BlogCart">
-        <p className='tag1'>Technology | Trends</p>
-            <h1 className='tag2'>Big Data In future</h1>
-            <p className='tag3'>Jane Smith</p>
-            <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br/>
-            do eiusmod tempor incididunt ut labore et dolore magna aliqua.. </p>
-        </div>
-        <div className="BlogCart">
-        <p className='tag1'>Technology | Trends</p>
-            <h1 className='tag2'>Big Data In future</h1>
-            <p className='tag3'>Jane Smith</p>
-            <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br/>
-            do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-
-        </div>
-        <div className="BlogCart">
-        <p className='tag1'>Technology | Trends</p>
+        <div className="details">
+            <p className='tag1'>Technology | Trends</p>
             <h1 className='tag2'>Big Data In future</h1>
             <p className='tag3'>Jane Smith</p>
             <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br/>
             do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-
+           <p className='tag5'><FaRegHeart/> <span></span> 600 <span><FaRegCommentDots/><span></span> 50</span></p> 
+           <div className='vl'></div>
+            <p className='tag6'>06 JUL 22</p>
+            </div>
         </div>
         <div className="BlogCart">
-        <p className='tag1'>Technology | Trends</p>
+        <div className="details">
+            <p className='tag1'>Technology | Trends</p>
             <h1 className='tag2'>Big Data In future</h1>
             <p className='tag3'>Jane Smith</p>
             <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br/>
             do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-
+           <p className='tag5'><FaRegHeart/> <span></span> 600 <span><FaRegCommentDots/><span></span> 50</span></p> 
+           <div className='vl'></div>
+            <p className='tag6'>06 JUL 22</p>
+            </div>
+        </div>
+        <div className="BlogCart">
+        <div className="details">
+            <p className='tag1'>Technology | Trends</p>
+            <h1 className='tag2'>Big Data In future</h1>
+            <p className='tag3'>Jane Smith</p>
+            <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br/>
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+           <p className='tag5'><FaRegHeart/> <span></span> 600 <span><FaRegCommentDots/><span></span> 50</span></p> 
+           <div className='vl'></div>
+            <p className='tag6'>06 JUL 22</p>
+            </div>
+        </div>
+        <div className="BlogCart">
+        <div className="details">
+            <p className='tag1'>Technology | Trends</p>
+            <h1 className='tag2'>Big Data In future</h1>
+            <p className='tag3'>Jane Smith</p>
+            <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed <br/>
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+           <p className='tag5'><FaRegHeart/> <span></span> 600 <span><FaRegCommentDots/><span></span> 50</span></p> 
+           <div className='vl'></div>
+            <p className='tag6'>06 JUL 22</p>
+            </div>
         </div>
     </div>
   )
