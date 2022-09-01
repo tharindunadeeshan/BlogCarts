c6dca24f64240f19bf0696789036b1f227171567
tharindunadeeshan <57563360+tharindunadeeshan@users.noreply.github.com>
2022-09-01 17:11:59 +0530

    Add Blur effect

--- src/components/BlogPage/BlogPageCart.jsx
+++ src/components/BlogPage/BlogPageCart.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import './BlogPageCart.css'
-
+import React from 'react';
+import './BlogPageCart.css';
+import b2  from '../../Images/2.png';
+import b3  from '../../Images/3.jpg';
 import { FaRegCommentDots,FaRegHeart } from "react-icons/fa";
+
 const BlogPageCart = () => {
   return (
     <div className="Blog-Container">
         <div className="BlogCart">
+        <div>
+    
+    </div>
             <p className='welcome'>Welcome to Our </p>
             <p className='Blogss'>Blogs</p>
             <p className='blogtag6'>07 JUL 2022</p>
@@ -13,14 +18,18 @@ const BlogPageCart = () => {
             
         </div>
         <div className="BlogCart">
+            <div className='bg-text'>
             <h1>Blog Articales</h1>
             <p>Sugessions</p>
+            </div>
             
+            
+         
           
             
         </div>
         <div className="BlogCart">
-            <div className="details">
+            <div className="bg-text">
             <p className='tag1'>Technology | Trends</p>
             <h1 className='tag2'>Big Data In future</h1>
             <p className='tag3'>Jane Smith</p>
@@ -30,71 +39,96 @@ const BlogPageCart = () => {
             <FaRegCommentDots className="icon" style={{ marginLeft:"34px", marginRight:"6px"}} size="17px"color="white" /><span></span> 50</span></p> 
            <div className='vl'></div>
             <p className='tag6'>06 JUL 22</p>
+            
             </div>
+            <div className='new'>
+                <img   className='backgroundimg' src={b2} />
+            </div>
+            
         </div>
         <div className="BlogCart">
-        <div className="details">
+        <div className="bg-text">
             <p className='tag1'>Technology | Trends</p>
             <h1 className='tag2'>Big Data In future</h1>
             <p className='tag3'>Jane Smith</p>
             <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
             do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-            <p className='tag5'><FaRegHeart   className="icon" style={{marginRight:"6px"}} size="17px"color="white"/> <span></span> 600 <span>
+           <p className='tag5'><FaRegHeart   className="icon" style={{marginRight:"6px"}} size="17px"color="white"/> <span></span> 600 <span>
             <FaRegCommentDots className="icon" style={{ marginLeft:"34px", marginRight:"6px"}} size="17px"color="white" /><span></span> 50</span></p> 
            <div className='vl'></div>
             <p className='tag6'>06 JUL 22</p>
+            
+            </div>
+            <div className='new'>
+                <img   className='backgroundimg' src={b3} />
             </div>
         </div>
         <div className="BlogCart">
-        <div className="details">
+        <div className="bg-text">
             <p className='tag1'>Technology | Trends</p>
             <h1 className='tag2'>Big Data In future</h1>
             <p className='tag3'>Jane Smith</p>
             <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
             do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-            <p className='tag5'><FaRegHeart   className="icon" style={{marginRight:"6px"}} size="17px"color="white"/> <span></span> 600 <span>
+           <p className='tag5'><FaRegHeart   className="icon" style={{marginRight:"6px"}} size="17px"color="white"/> <span></span> 600 <span>
             <FaRegCommentDots className="icon" style={{ marginLeft:"34px", marginRight:"6px"}} size="17px"color="white" /><span></span> 50</span></p> 
            <div className='vl'></div>
             <p className='tag6'>06 JUL 22</p>
+            
+            </div>
+            <div className='new'>
+                <img   className='backgroundimg' src={b3} />
             </div>
         </div>
         <div className="BlogCart">
-        <div className="details">
-            <p className='tag1'>Technology | Trends</p>
-            <h1 className='tag2'>Big Data In future</h1>
-            <p className='tag3'>Jane Smith</p>
-            <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-            do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-            <p className='tag5'><FaRegHeart   className="icon" style={{marginRight:"6px"}} size="17px"color="white"/> <span></span> 600 <span>
-            <FaRegCommentDots className="icon" style={{ marginLeft:"34px", marginRight:"6px"}} size="17px"color="white" /><span></span> 50</span></p>  
-           <div className='vl'></div>
-            <p className='tag6'>06 JUL 22</p>
-            </div>
-        </div>
-        <div className="BlogCart">
-        <div className="details">
+        <div className="bg-text">
             <p className='tag1'>Technology | Trends</p>
             <h1 className='tag2'>Big Data In future</h1>
             <p className='tag3'>Jane Smith</p>
             <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
             do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-            <p className='tag5'><FaRegHeart   className="icon" style={{marginRight:"6px"}} size="17px"color="white"/> <span></span> 600 <span>
-            <FaRegCommentDots className="icon" style={{ marginLeft:"34px", marginRight:"6px"}} size="17px"color="white" /><span></span> 50</span></p>  
-           <div className='vl'></div>
-            <p className='tag6'>06 JUL 22</p>
-            </div>
-        </div>
-        <div className="BlogCart">
-        <div className="details">
-            <p className='tag1'>Technology | Trends</p>
-            <h1 className='tag2'>Big Data In future</h1>
-            <p className='tag3'>Jane Smith</p>
-            <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
-            do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-            <p className='tag5'><FaRegHeart   className="icon" style={{marginRight:"6px"}} size="17px"color="white"/> <span></span> 600 <span>
+           <p className='tag5'><FaRegHeart   className="icon" style={{marginRight:"6px"}} size="17px"color="white"/> <span></span> 600 <span>
             <FaRegCommentDots className="icon" style={{ marginLeft:"34px", marginRight:"6px"}} size="17px"color="white" /><span></span> 50</span></p> 
            <div className='vl'></div>
             <p className='tag6'>06 JUL 22</p>
+            
+            </div>
+            <div className='new'>
+                <img   className='backgroundimg' src={b2} />
+            </div>
+        </div>
+        <div className="BlogCart">
+        <div className="bg-text">
+            <p className='tag1'>Technology | Trends</p>
+            <h1 className='tag2'>Big Data In future</h1>
+            <p className='tag3'>Jane Smith</p>
+            <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+           <p className='tag5'><FaRegHeart   className="icon" style={{marginRight:"6px"}} size="17px"color="white"/> <span></span> 600 <span>
+            <FaRegCommentDots className="icon" style={{ marginLeft:"34px", marginRight:"6px"}} size="17px"color="white" /><span></span> 50</span></p> 
+           <div className='vl'></div>
+            <p className='tag6'>06 JUL 22</p>
+            
+            </div>
+            <div className='new'>
+                <img   className='backgroundimg' src={b2} />
+            </div>
+        </div>
+        <div className="BlogCart">
+        <div className="bg-text">
+            <p className='tag1'>Technology | Trends</p>
+            <h1 className='tag2'>Big Data In future</h1>
+            <p className='tag3'>Jane Smith</p>
+            <p className='tag4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+           <p className='tag5'><FaRegHeart   className="icon" style={{marginRight:"6px"}} size="17px"color="white"/> <span></span> 600 <span>
+            <FaRegCommentDots className="icon" style={{ marginLeft:"34px", marginRight:"6px"}} size="17px"color="white" /><span></span> 50</span></p> 
+           <div className='vl'></div>
+            <p className='tag6'>06 JUL 22</p>
+            
+            </div>
+            <div className='new'>
+                <img   className='backgroundimg' src={b3} />
             </div>
         </div>
     </div>
